990ca85425da32b87af5f90c36757c8e92fa6e1f
afidhu <afidhumatata81@gmail.com>
2026-02-05 12:57:57 +0300

    modified:   medicine_locator_for_pharmacies/lib/features/medicines/presentation/widgets/medicine_card.dart

--- medicine_locator_for_pharmacies/lib/features/medicines/presentation/screens/all_medicines.dart
+++ medicine_locator_for_pharmacies/lib/features/medicines/presentation/screens/all_medicines.dart
@@ -2,13 +2,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class AllMedicines extends StatelessWidget {
+class AllMedicines extends StatefulWidget {
   const AllMedicines({super.key});
 
   @override
+  State<AllMedicines> createState() => _AllMedicinesState();
+}
+
+class _AllMedicinesState extends State<AllMedicines> {
+  var isSelected =false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(),
       body: SafeArea(
           child: Container(
@@ -23,7 +29,7 @@ class AllMedicines extends StatelessWidget {
               return Container(
                 height: 300.h,
                 width: double.infinity,
-                padding: EdgeInsets.all(10),
+                // padding: EdgeInsets.all(10),
                 margin: EdgeInsets.all(10),
                 decoration: BoxDecoration(
                     // color: Colors.grey[300],
@@ -45,30 +51,86 @@ class AllMedicines extends StatelessWidget {
                     Expanded(
                       child: ListView.builder(controller: scrollController,
                       itemBuilder: (context, index){
-                        return ListTile(
-                          minLeadingWidth: 10.w,
-                          h
-                          leading: Container(
-                            width: 100.w,
-                            height: 100.h,
-                              margin: EdgeInsets.only(top: 5),
-                              padding: EdgeInsets.all(5),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                color: Colors.red
+                        return Container(
+                          margin: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.white60,
+                            borderRadius: BorderRadius.circular(10),
+                            shape: BoxShape.rectangle,
+                            boxShadow: [
+                              BoxShadow(
+                                  blurRadius: 1,
+                                  color: Colors.grey.shade300,
+                                  offset: Offset(0, 10),
+                                  spreadRadius: 1
+                              )
+                            ]
+                          ),
+                          child: ListTile(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                            minVerticalPadding: 8,
+                            contentPadding: EdgeInsets.all(10),
+                            minLeadingWidth: 100.w,
+                            horizontalTitleGap: 100.w,
+                            minTileHeight: 100,
+                            title: Container(
+                              width: 100.w,
+                              height: 100.h,
+                                margin: EdgeInsets.only(top: 5,bottom: 5),
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                  color: Colors.white38
+                                ),
+                                child: Stack(
+                                  children: [
+                                    Positioned(
+                                      top: 0,
+                                      left: 0,
+                                      right: 0,
+                                      child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: Image.network('https://picsum.photos/250?image=$index',width: 100.w,height: 80.h,
+                                        fit: BoxFit.cover,
+                                        loadingBuilder: (context, child, loadingProgress) {
+                                          if (loadingProgress == null) return child;
+                                          return Center(
+                                            child: CircularProgressIndicator()
+                                          );
+                                        }
+                                      )
+                                      ),
+                                    ),
+                                    Positioned(
+                                        right: 5,
+                                        child: RawChip(
+                                          selectedColor: Colors.red,
+                                            selected: isSelected,
+                                            onSelected: (val){
+                                              setState(() {
+                                                isSelected=!isSelected;
+                                              });
+                                            print(val);
+                                            },
+                                            backgroundColor: Colors.white,
+                                            label: Text('select')))
+                                  ],
+                                )),
+                            // title: Text('Medicine $index'),
+                            subtitle: ExpansionTile(title: Text('Medicine $index',style: TextStyle(fontWeight: FontWeight.bold),),
+                              collapsedShape: ShapeBorder.lerp(
+                                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)), 0
                               ),
-                              child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.network('https://picsum.photos/250?image=$index',width: 100.w,height: 80.h,
-                                fit: BoxFit.fitWidth,
-                                loadingBuilder: (context, child, loadingProgress) {
-                                  if (loadingProgress == null) return child;
-                                  return Center(
-                                    child: CircularProgressIndicator()
-                                  );
-                                }
-                              ))),
-                          title: Text('Medicine $index'),
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                              dense: true,
+                               children: [
+                                 Text('Destcribunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn'),
+                               ],
+
+                            ),
+                          ),
                         );
                       })
                     ),
