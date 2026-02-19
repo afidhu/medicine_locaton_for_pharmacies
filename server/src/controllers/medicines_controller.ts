61672f7a69f1aaee6a959f0a3e29797720e4b620
afidhu <afidhumatata81@gmail.com>
2026-02-20 00:33:27 +0300

    fiexed

--- server/src/controllers/medicines_controller.ts
+++ server/src/controllers/medicines_controller.ts
@@ -1,9 +1,20 @@
 import type { Request, Response } from "express";
 
 
+
 export const addMedicine = (req:Request, res:Response) =>{
-    const { name, description, price } = req.body;
-    // const resukts = prism
+    console.log('hellow')
+    res.send('Medicine added successfully!');
+    // const { name, description, price,type } = req.body;
+    // const resukts = prisma.medicines.create({
+    //     data:{
+    //         description: description,
+    //         name: name,
+    //         price: price,
+    //         imageUrl: "https://example.com/medicine-image.jpg", // You can replace this with an actual image URL or handle it as needed
+    //         type: "tablet" // You can replace this with the actual type of medicine or handle it as needed
+    //     }
+    // })
     // Here you would typically add the medicine to your database
-    res.status(201).json({ message: "Medicine added successfully", medicine: { name, description, price } });
+
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -7,15 +7,22 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
-// const connectionString = `${process.env.DATABASE_URL}`;
-// const adapter = new PrismaPg({ connectionString });
-// const prisma = new PrismaClient({ adapter });
+import { PrismaClient } from '@prisma/client';
+import medicine_routers from './routes/medicine_routers.ts';
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
-// export { prisma };
+export { prisma };
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+
+app.use(express.json());
+
+app.use('/medicines', medicine_routers)
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
